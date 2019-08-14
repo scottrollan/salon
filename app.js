1c4145735b5eefdabfaa69e8432bc7aa1bd31058
Barry Scott Rollan <barry.rollan@gmail.com>
2019-08-14 09:46:48 -0400

    changed functions and styling of the readMore and readLess botton on blog jumbotron

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 $(function(){
 
-    readMore( $('.spoiler'), 4);
-
-  function readMore(jObj, lineNum) {
-    if ( isNaN(lineNum) ) {
-      lineNum = 4;
-    }
-    var go = new ReadMore (jObj, lineNum);
-  }
-
-  //class
-  function ReadMore(_jObj, lineNum) { 
-    var READ_MORE_LABEL = 'read more';
-    var HIDE_LABEL = 'minimize';
-
-    var jObj = _jObj;
-    var textMinHeight = ''+ (parseInt(jObj.children('.hidden-text').css('line-height'),10)*lineNum) +'px';
-    var textMaxHeight = ''+jObj.children('.hidden-text').css('height');
-
-    jObj.children('.hidden-text').css('height', ''+ textMaxHeight);
-    jObj.children('.hidden-text').css( 'transition', 'height .5s');
-    jObj.children('.hidden-text').css('height', ''+ textMinHeight);
-
-    jObj.append ('<button class="read-more">'+READ_MORE_LABEL+'</button>');
-
-    jObj.children('.read-more').click ( function() {
-      if (jObj.children('.hidden-text').css('height') === textMinHeight) {
-        jObj.children('.hidden-text').css('height', ''+textMaxHeight);
-        jObj.children('.read-more').html(HIDE_LABEL).addClass('active');
-      } else {
-        jObj.children('.hidden-text').css('height', ''+textMinHeight);
-        jObj.children('.read-more').html(READ_MORE_LABEL).removeClass('active');
-      }
-    });
-
-  }
+  $("#seeMore").click(function() {
+    $(".reveal").show();
+    $("#seeMore").hide();
+  });
+  
+  $("#seeLess").click(function() {
+    $(".reveal").hide();
+    $("#seeMore").show();
+  });
   
   $("#openMap").click(function() {
     $("#map").show();
